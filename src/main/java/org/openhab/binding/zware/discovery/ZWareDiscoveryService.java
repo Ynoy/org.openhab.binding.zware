9bc208c4e9308b38fd5e9e209321389e94a6497f
Ynoy <xy410257@163.com>
2018-05-26 17:22:50 +0800

    add Http Request Util with cookies!!!

--- src/main/java/org/openhab/binding/zware/discovery/ZWareDiscoveryService.java
+++ src/main/java/org/openhab/binding/zware/discovery/ZWareDiscoveryService.java
@@ -8,23 +8,22 @@ import org.eclipse.smarthome.config.discovery.DiscoveryResult;
 import org.eclipse.smarthome.config.discovery.DiscoveryResultBuilder;
 import org.eclipse.smarthome.config.discovery.DiscoveryService;
 import org.eclipse.smarthome.config.discovery.DiscoveryServiceCallback;
-import org.eclipse.smarthome.config.discovery.ExtendedDiscoveryService;
 import org.eclipse.smarthome.core.thing.ThingUID;
 import org.openhab.binding.zware.handler.ZWareBridgeHandler;
 import org.openhab.binding.zware.internal.ZWareBindingConstants;
 import org.openhab.binding.zware.utils.HttpUtils;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;;
+import org.slf4j.LoggerFactory;
+
+import com.hao.httputil.OkHttpUtils;;
 
 @Component(service = DiscoveryService.class, immediate = true, configurationPid = "discovery.zware")
-public class ZWareDiscoveryService extends AbstractDiscoveryService implements ExtendedDiscoveryService {
+public class ZWareDiscoveryService extends AbstractDiscoveryService {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private static final int SEARCH_TIME = 60 * 4;
-
-    private HttpUtils httpUtils;
 
     private DiscoveryServiceCallback discoveryServiceCallback;
 
@@ -42,56 +41,46 @@ public class ZWareDiscoveryService extends AbstractDiscoveryService implements E
         super(ZWareBindingConstants.SUPPORTED_DEVICE_THING_TYPES_UIDS, SEARCH_TIME);
     }
 
-    public void activate() {
-        logger.debug("Activating ZWave discovery service for {}", controllerHandler.getThing().getUID());
-    }
-
-    @Override
-    public void deactivate() {
-        logger.debug("Deactivating ZWave discovery service for {}", controllerHandler.getThing().getUID());
-    }
-
-    private void scan(String type) {
+    private void scan() {
         // TODO Auto-generated method stub
-        String url = ZWareBindingConstants.hosts + type;
+        String url = ZWareBindingConstants.hosts + ZWareBindingConstants.URL_INCLUDE_EXCLUDE;
         Map<String, String> map = new HashMap<>();
-
-        if (type.equals(ZWareBindingConstants.URL_INCLUDE_EXCLUDE)) {
-            map.put("cmd", "2");
-            map.put("&", "");
-            String resp = HttpUtils.httpPost(url, null);
-            if (resp != null) {
-                thingDiscovered(null);
-            }
-            logger.error(HttpUtils.httpPost(ZWNET_GET_NODESLIST, null));
-            ThingUID thingUID = new ThingUID(ZWareBindingConstants.ZWAVE_THING_UID, "node1");
-
-            // Attention: if is already present as thing in the ThingRegistry
-            // the configuration for thing will be updated!
-            DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID)
-                    .withProperty(ZWareBindingConstants.User, "zware").withLabel("Zware Device").build();
-            thingDiscovered(discoveryResult);
-        }
-
+        HttpUtils httpUtils = new HttpUtils();
+        map.put("cmd", "2");
+        map.put("&", "");
         String resp = HttpUtils.httpPost(url, null);
-        thingDiscovered(null);
+
+        logger.error(HttpUtils.httpPost(ZWNET_GET_NODESLIST, null));
+        ThingUID thingUID = new ThingUID(ZWareBindingConstants.ZWAVE_THING_UID, "node1");
+
+        // Attention: if is already present as thing in the ThingRegistry
+        // the configuration for thing will be updated!
+        DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID)
+                .withProperty(ZWareBindingConstants.User, "zware").withLabel("Zware Device").build();
+        thingDiscovered(discoveryResult);
 
     }
 
     @Override
     protected void startScan() {
-        scan(ZWareBindingConstants.URL_INCLUDE_EXCLUDE);
+        scan();
     }
 
     @Override
     protected synchronized void stopScan() {
-        scan(ZWareBindingConstants.URL_ABORT_NETWORK);
+        stop();
         super.stopScan();
+        removeOlderResults(getTimestampOfLastScan());
+
+    }
+
+    private void stop() {
+        // TODO Auto-generated method stub
     }
 
     @Override
     public synchronized void abortScan() {
-        scan(ZWareBindingConstants.URL_ABORT_NETWORK);
+        stop();
         super.abortScan();
     }
 
@@ -113,9 +102,4 @@ public class ZWareDiscoveryService extends AbstractDiscoveryService implements E
         }
     }
 
-    @Override
-    public void setDiscoveryServiceCallback(DiscoveryServiceCallback discoveryServiceCallback) {
-        // TODO Auto-generated method stub
-        this.discoveryServiceCallback = discoveryServiceCallback;
-    }
 }
---
+++ src/main/java/org/openhab/binding/zware/utils/OkHttpUtils.java
@@ -0,0 +1,132 @@
+package org.openhab.binding.zware.utils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.openhab.binding.zware.internal.ZWareBindingConstants;
+
+import okhttp3.Call;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Http Request Util with cookies!!!
+ *
+ * @author: Honey
+ * @date: 2018年5月24日
+ * @tags:
+ * @email: xy410257@163.com
+ */
+public class OkHttpUtils {
+
+    // 初始化Cookie管理器
+    static CookieJar cookieJar = new CookieJar() {
+        // Cookie缓存区
+        private final Map<String, List<Cookie>> cookiesMap = new HashMap<String, List<Cookie>>();
+
+        @Override
+        public void saveFromResponse(HttpUrl arg0, List<Cookie> arg1) {
+            // TODO Auto-generated method stub
+            // 移除相同的url的Cookie
+            String host = arg0.host();
+            List<Cookie> cookiesList = cookiesMap.get(host);
+            if (cookiesList != null) {
+                cookiesMap.remove(host);
+            }
+            // 再重新天添加
+            cookiesMap.put(host, arg1);
+        }
+
+        @Override
+        public List<Cookie> loadForRequest(HttpUrl arg0) {
+            // TODO Auto-generated method stub
+            List<Cookie> cookiesList = cookiesMap.get(arg0.host());
+            // 注：这里不能返回null，否则会报NULLException的错误。
+            // 原因：当Request 连接到网络的时候，OkHttp会调用loadForRequest()
+            return cookiesList != null ? cookiesList : new ArrayList<Cookie>();
+        }
+    };
+
+    private static OkHttpClient client = new OkHttpClient.Builder().connectTimeout(15, TimeUnit.SECONDS)
+            // 添加OkHttp3的拦截器
+            .writeTimeout(20, TimeUnit.SECONDS).readTimeout(20, TimeUnit.SECONDS).cookieJar(cookieJar).build();;
+
+    public static void postRequest(String urlParams, Map<String, String> mapParams) {
+        // 创建登陆的表单
+        FormBody loginBody = new FormBody.Builder().add("usrname", "sigma").add("passwd", "sigmadesigns").build();// 账号密码自己填
+
+        Request loginRequest = new Request.Builder().url(ZWareBindingConstants.hosts + ZWareBindingConstants.URL_LOGIN)
+                .post(loginBody).build();
+        // 获取需要提交的CookieStr
+        StringBuilder cookieStr = new StringBuilder();
+        // 从缓存中获取Cookie
+        List<Cookie> cookies = client.cookieJar().loadForRequest(loginRequest.url());
+        System.out.println(cookies);
+        if (cookies.isEmpty()) {
+            System.out.println("cookies==null");
+            // 上传
+            Call loginCall = client.newCall(loginRequest);
+
+            try {
+                // 非异步执行
+                Response loginResponse = loginCall.execute();
+                // 测试是否登陆成功
+                System.out.println(loginResponse.body().string());
+                // 获取返回数据的头部
+                Headers headers = loginResponse.headers();
+                HttpUrl loginUrl = loginRequest.url();
+                // 获取头部的Cookie,注意：可以通过Cooke.parseAll()来获取
+                cookies = Cookie.parseAll(loginUrl, headers);
+                // 防止header没有Cookie的情况
+                if (cookies != null) {
+                    // 存储到Cookie管理器中
+                    client.cookieJar().saveFromResponse(loginUrl, cookies);// 这样就将Cookie存储到缓存中了
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+        cookies = client.cookieJar().loadForRequest(loginRequest.url());
+
+        // 将Cookie数据弄成一行
+        for (Cookie cookie : cookies) {
+            cookieStr.append(cookie.name()).append("=").append(cookie.value() + ";");
+        }
+
+        // 设置提交的请求
+        FormBody.Builder builder = new FormBody.Builder();
+        // 遍历集合
+        for (String key : mapParams.keySet()) {
+            builder.add(key, mapParams.get(key));
+        }
+        Request attentionRequest = new Request.Builder().url(urlParams).header("Cookie", cookieStr.toString())
+                .post(builder.build()).build();
+        Call attentionCall = client.newCall(attentionRequest);
+        try {
+            // 连接网络
+            Response attentionResponse = attentionCall.execute();
+            if (attentionResponse.isSuccessful()) {
+                // 获取返回的数据
+                String data = attentionResponse.body().string();
+                // 测试
+                System.out.println(data);
+
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
